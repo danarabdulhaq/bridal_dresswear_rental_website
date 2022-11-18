91ebac199d0bf4d5cd09f200d5a774ed478d55b7
ReemaKusa <s12112910@stu.najah.edu>
2022-11-18 21:54:53 +0300

    rrr

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,18 +7,20 @@ import img3 from'./images/M3.jpg'
 const Contact = () => {
     return(
         <div className="container" >
+           <div className="hwfbwjf">
             <h2 className="paragraph">Contact Us</h2>
-            <input className="name" type="text" class="fieldN" placeholder="Your Name">
-            </input>
-            <input className="email" type="text" class="fieldE" placeholder="Your Email">
-            </input>
+            <input className="name" type="text" class="fieldN" placeholder="Your Name"></input>
+            <input className="email" type="text" class="fieldE" placeholder="Your Email"></input>
             <input className="phone" type="text" class="fieldP" placeholder="Phone">
-           </input> <textarea placeholder="Message" class="field"></textarea>
+            </input> <textarea placeholder="Message" class="field"></textarea>
             <button className="btn">Send</button>
             <img className="image" src={img3} alt="please wait my beautiful girl"></img>
+            <button type="submit">Send</button>
+            </div>
         </div>
     
     )
  }
+ 
  // export
 export default Contact

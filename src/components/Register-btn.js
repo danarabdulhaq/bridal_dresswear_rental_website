a43855cd8dde2e0016e206053da6c2263d3335ea
MarahNatsheh <117008939+MarahNatsheh@users.noreply.github.com>
2022-11-18 19:49:30 +0200

    Update Register-btn.js

--- src/components/Register-btn.js
+++ src/components/Register-btn.js
@@ -3,11 +3,11 @@ import './Register-btn.css'
 
 const Btn =(props)=>
 {
-  const title =(props.title)? props :"Login"
+  
 
     return(
         <div className="pt-1 mb-4">
-        <button className="btn  btn-lg btn-block" type="button" id="registerBtn">{title}</button>
+        <button className="btn  btn-lg btn-block" type="button" id="registerBtn">{props.title}</button>
       </div>);
 }
 export default Btn;

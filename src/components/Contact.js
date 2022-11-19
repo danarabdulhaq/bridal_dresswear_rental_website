208bdf601337eab3a4258fb564ce1836ac5ba1b5
ReemaKusa <s12112910@stu.najah.edu>
2022-11-19 16:56:53 +0300

    rrr

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,20 +6,22 @@ import img3 from'./images/M3.jpg'
 //function
 const Contact = () => {
     return(
+	
         <div className="container">
 		   <div className="contact-box">
-		 	   <div className="left"></div>
+		      <div className="left">
+			  <img className="imageThree" src={img3} alt="please wait my beautiful girl"></img>
+			  </div>
 			     <div className="right">
 				 <h2>Contact Us</h2>
 				 <input type="text" className="field" placeholder="Your Name"></input>
 				 <input type="text" className="field" placeholder="Your Email"></input>
-				 <input type="text" className="field" placeholder="Phone"></input>
+				 <input type="text" className="field" placeholder="Your Phone"></input>
 				 <textarea placeholder="Your Message PLS" className="field"></textarea>
 				 <button className="btn">Send</button>
-                 <img className="imageTwo" src={img3} alt="please wait my beautiful girl"></img>
 			</div>
 		</div>
-	</div>
+	   </div>
     );
  }
 

95b8e1657df3650d7c037b4f780024072c3f1622
ReemaKusa <s12112910@stu.najah.edu>
2022-11-19 18:21:55 +0300

    rrr

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import './App.css';
 import React from 'react';
 import Design from './components/Design'; 
 import Contact from './components/Contact'; 
-
+//import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
 
-  <>
+<>
+   <Design/>
+   <Contact/>
+  </>
+   
+);
+}
+ 
+export default App;
+
+
+
+
+
+
+/*function App() {
+  return (
+<>
    <Design/>
    <Contact/>
   </>
 
+
   );
-}
- 
-export default App;
+}*/

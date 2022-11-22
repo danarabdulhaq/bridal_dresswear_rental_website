f556c06a93d7c0036bbd6b0afedfefc50b5f07e3
EmanSOthman <eman.s.s.otman@gmail.com>
2022-11-22 23:29:49 +0200

    services pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import './components/Service';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Design from './components/Design';
 
 function App() {
   return (
-   <Design/>
+
+
+    <BrowserRouter>
+
+    <Routes>
+
+              <Route path='/' element={<Service/>}/>
+              <Route index path='/Service' element ={<Service/>}/>
+
+    </Routes>
+
+
+</BrowserRouter>
+
+
   );
 }
 

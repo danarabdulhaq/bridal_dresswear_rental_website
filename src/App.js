e82c2805b5e533238db731970a21b89a5dafa225
danaabdulhaq <danarabdulhaq@gmail.com>
2022-11-30 14:49:30 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
-import About from './components/Pages/About';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 
 function App() {
   return (
 
-<BrowserRouter>
-
-        <Routes>
-
-                  <Route path='/' element={<About/>}/>
-                  <Route index path='/pages/About' element ={<About/>}/>
-
-        </Routes>
+<div>
 
 
-</BrowserRouter>
-
-
+</div>
   );
 }
 

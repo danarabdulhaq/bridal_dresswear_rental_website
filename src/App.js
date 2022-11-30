563ebacd8fbb4b85f9c6bc74bf5da176fb9c8e6e
ReemaKusa <s12112910@stu.najah.edu>
2022-11-30 16:20:17 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Contact from './components/Contact';
 import Card from './components/Card';
 //import footer
 import Footer from './components/footer';
-//import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -21,21 +20,3 @@ function App() {
  
 export default App;
 
-
-
-////////////////////////////////////////////////////////
-/*function App() {
-  return (
-<BrowserRouter>
- <Routes>
-
-          <Route path='/' element={<Design/>}/>
-          <Route index path='/pages/About' element ={<Design/>}/>
-          <Route path='/' element={<Contact/>}/>
-          <Route index path='/pages/About' element ={<Contact/>}/>
-
- </Routes>
-</BrowserRouter>
-
-);
-}*/

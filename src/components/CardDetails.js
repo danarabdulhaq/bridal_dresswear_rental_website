cdf18c42f8de1d67fb0ec313a2d4db4931119a16
ReemaKusa <s12112910@stu.najah.edu>
2022-12-01 21:42:33 +0300

    All item details

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -7,14 +7,33 @@ const CardDetails = () => {
   return(
 	
 	<div className="container" >
-		<div className="details">
-		    <div className="leftt"></div>
+		<div className="details-box">
+		     <div className="leftt">
+			 </div>
 		    <div className="rightt">
-                   <h5 className="card-title">Description for item ...</h5>
-			       <p className="card-text"> Description of the piece here in terms of length, width, description of materials, design, designer, etc .</p>
-			       <p className="card-text-t"><small>Size : M</small></p>
-				   <p className="card-text-c"><small>Color : White</small></p>
-				   <button  className="btn-rent"> Rent Now </button>
+                   <h5 className="card-title">Description for item</h5>
+
+				   <div className="description">
+			          <p className="card-text"> Description of the piece here in terms of length, width, description of materials, design, designer, etc .</p>
+				   </div>
+
+                   <div className="size">
+			         <p className="card-text">Size : M</p>
+				   </div>
+
+				   <div className="sizeNum">
+                      <p className="card-text"><small>length : 130 cm</small></p>
+				      <p className="card-text"><small>Waist : 74 cm</small></p>
+				   </div>
+
+				   <div className="sizesNum">
+				     <p className="card-text"><small>Shoulders : 70 cm</small></p>
+				     <p className="card-text"><small>Bust : 94 cm</small></p>
+				   </div>
+                    <div className="color">
+				     <p className="card-text">Color : White</p>
+					 </div>
+				   <button className="btn-rent"> Rent Now </button>
 		    </div>
 		</div>
 	</div>  

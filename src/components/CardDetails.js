8f3dd74ed5d2e52c9dc72a8a022e8c1aca8bddc8
ReemaKusa <s12112910@stu.najah.edu>
2022-12-01 15:14:17 +0300

    All item details

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,28 +1,24 @@
 // import
 import React from "react"
 import './CardDetails.css'
-import CDI from './images/testCardDetailsImg.jpg';
-
-
 //function
 
 const CardDetails = () => {
-    return(
+  return(
 	
-        <div className="container">
-		   <div className="details-box">
-		      <div className="leftt">
-			   <img className="CDI" src={CDI} alt="..."></img></div>
-			    <div className="rightt">
-				 <h3>Description for item ... </h3>
-				 <h5> Description of the piece here in terms of length, width, description of materials, design, designer, etc </h5>
-				 <h4>Size : M</h4>
-				 <h4>Color : White</h4>
-				 <button className="rent-btn"> Rent Now </button>
-			    </div>
-		   </div>
-	   </div>
-    );
+	<div className="container" >
+		<div className="details">
+		    <div className="leftt"></div>
+		    <div className="rightt">
+                   <h5 className="card-title">Description for item ...</h5>
+			       <p className="card-text"> Description of the piece here in terms of length, width, description of materials, design, designer, etc .</p>
+			       <p className="card-text-t"><small>Size : M</small></p>
+				   <p className="card-text-c"><small>Color : White</small></p>
+				   <button  className="btn-rent"> Rent Now </button>
+		    </div>
+		</div>
+	</div>  
+  );
 }
 
 

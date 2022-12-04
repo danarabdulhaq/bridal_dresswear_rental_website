5821181cf001f434bac5b940acac3e7a94de99a3
danaabdulhaq <danarabdulhaq@gmail.com>
2022-12-04 23:09:48 +0200

    last modifying

--- src/components/Pages/Aboutus.js
+++ src/components/Pages/Aboutus.js
@@ -12,39 +12,44 @@ const About = () => {
                 
                                     
         <div className="container"> 
-                <div className='border-for-about'>       
+                <div className='border-for-about padd'>       
                         <h1 className='tithead'>About us</h1>
                             <h3>
                             " Welcome dear La Belle , We hope to gain your trust Get to know us "
                             </h3>
-                            <img className="img-circle-head" src={require('./Display_dresse.jpg')} alt="" />
+                            
+                            <img className=" img-circle-head img-fluid"  src={require('./Display_dresse.jpg')} alt="" />
 
                                             
-                                  <div class="container text-center">
-                                            <div class="row">
-                                            <div class="col-sm-4">
-                                                                    <img className='img-circle' src={require('./img-history.jpg')} alt ="image"  />
+                                  <div className="container text-center padd ">
+                                         <div className ="row-abuotus">
+                                            <div className ="col-sm-4 pb-1">
+                                                                    <img className='img-circle img-fluid' src={require('./img-history.jpg')} alt ="image1"  />
                                                             
-                                                            <h2> history :</h2>
+
 
                                                                 <div>
-                                                                    <p> La Belle Company was established in 2019. During this period, we have been able to develop our website so that we can rent bridal dresses remotely, with good quality and great trust between us,
+                                                                <h2 className="tit-his"> History :</h2>
+
+                                                                    <p className="text-l" > La Belle Company was established in 2019. During this period, we have been able to develop our website so that we can rent bridal dresses remotely, with good quality and great trust between us,
                                                                                         During this period, and with the spread of Corona disease, the demand increased through our website for renting dresses, to be a helpful factor in identifying us by the community</p>
                                                         
                                                                 </div>             
                                                      </div>
                                                 </div>
 
-                                            <div class="row">
-                                                <div class="col-sm">
+                                            <div className="row-abuotus ">
+                                                <div className ="col-sm pb-1">
                                                                              
-                                                    <img className='img-circle-our-features' src={require('./img- Our features.jpg')} alt ="image"  />
+                                                    <img className='img-circle-our-features img-fluid' src={require('./img- Our features.jpg')} alt ="image2"  />
                                                     
-                                                        <h2>  Our features :</h2>
-                       
+                                                      
+
                                                            <div>
-                                                             <p>
-                                                                   We save time and effort and ease of booking and receiving the order.
+                                                           <h2 className="tit-fec">  Our features :</h2>
+
+                                                                    <p className="text-r">
+                                                                        We save time and effort and ease of booking and receiving the order.
                                                                        There is ease in booking with complete clarity with Tfaasy. Also, there are a large number of options available from different designs and prices, and we provide all the needs of the bride from one place and with the click of a button, and we have easy electronic payment methods for everyone</p>
                                                            </div>
                                                  </div>
@@ -56,7 +61,7 @@ const About = () => {
                                      <div>
                                             <h2 className='padd'>Our team member </h2>
 
-                                            <div className='row ' > 
+                                            <div className='row rowCard' > 
                                                 <Teammember name = 'Sophia' identification = 'fashion designer from America'/>
                                                 <Teammember name = 'Raya' identification = 'fashion designer from Palestine'/>
                                                 <Teammember name = 'Eliana' identification = 'fashion designer from France'/>

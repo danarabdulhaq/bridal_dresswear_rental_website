300fb7b9199f9dd72f0a8bfaeb0ad79b7e45fede
danaabdulhaq <danarabdulhaq@gmail.com>
2022-12-04 23:30:16 +0200

    Merge branch 'main' of https://github.com/danaabdulhaq/bridal_dresswear_rental_website into danar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import About from './components/Pages/Aboutus';
+// import DisplayDress from './components/DisplayDress';
+//Components_fill_card
+// import Calendar from './components/Calendar'
+
 function App() {
   return (
 
     
       <>
+      {/* <DisplayDress/>      */}
+    
+      {/* <Calendar/> */}
 
-    
-    
+
+     <About/>
+
+     
       </>
 
 

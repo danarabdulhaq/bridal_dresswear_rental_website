743e999fdd3f4628b6b0e4066b93dfbf6e63131e
ReemaKusa <s12112910@stu.najah.edu>
2022-12-05 16:51:21 +0300

    Contact us page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,20 +7,18 @@ const Contact = () => {
     return(
 		
 	<div className="container">
-
 		<div className="contact-box">
-
-		 <div className="left"></div>
+		<div className="left"></div>
 			<div className="right">
 			  <h2>Contact Us</h2>
 			  <input type="text" className="field" placeholder="Your Name"></input>
 			  <input type="text" className="field" placeholder="Your Email"></input>
 			  <input type="text" className="field" placeholder="Your Phone"></input>
 			  <textarea placeholder="Your Message PLS" className="field"></textarea>
-			  <button className="btn">Send</button>
+			  <a href="..." className="btn">Send</a>
+
 		    </div>
 	    </div>
-
 	</div>
     );
  }

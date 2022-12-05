89eb91b03f4334ed3959e52447eb70005e41a197
ReemaKusa <s12112910@stu.najah.edu>
2022-12-05 16:59:23 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import './App.css';
-import React from 'react';
-import Contact from './components/Contact'; 
-//import footer
-import Footer from './components/footer';
-//import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Cal from './components/Calendar'
-import CardDetails from './components/CardDetails';
 
-function App() {
+import React from 'react';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Pages/Home';
+import Services from './components/Pages/Services';
+import AboutUs from './components/Pages/AboutUs';
+import {  Route, Routes } from 'react-router-dom';
+import Login from './components/Pages/LoginPage';
+import Register from './components/Pages/RegisterPage';
+import Contact from './components/Pages/ContactUs';
+
+
+ function App() {
   return (
 
-<>
-  
-   <Contact/>
-   <Cal/>
-   <CardDetails/>
-   <Footer/>
-  </>
-   
-   
-);
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+          <Route path="/ContactUs" element={<Contact />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+
+      </Routes>
+      <Footer/>
+</>
+  );
+
 }
+
+
  
 export default App;
-
-
-
-////////////////////////////////////////////////////////
-/*function App() {
-  return (
-<BrowserRouter>
- <Routes>
-
-          <Route path='/' element={<Design/>}/>
-          <Route index path='/pages/About' element ={<Design/>}/>
-          <Route path='/' element={<Contact/>}/>
-          <Route index path='/pages/About' element ={<Contact/>}/>
-
- </Routes>
-</BrowserRouter>
-
-);
-}*/

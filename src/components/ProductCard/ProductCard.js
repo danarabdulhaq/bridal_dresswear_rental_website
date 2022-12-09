0cf71b625bba55790360b48c48040050cfb9d07d
unknown <natsheh.marah22@gmail.com>
2022-12-10 01:45:55 +0200

    pages Card component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -13,17 +13,17 @@ const ProductCard = (props)=> {
       });
     });}
     return(
-        <div >
-        <div className="scene scene--card "  onClick={flipp}>
-  <div className="card">
+      <div className= "col-12 col-md-6 col-lg-4 col-xxl-3">
+  <div className="card" onClick={flipp}>
     <div className="card__face card__face--front"><img src={props.img} className="d-flex-row w-100" alt="..."/></div>
-    <div className="card__face card__face--back"><div className="card-back">
+    <div className="card__face card__face--back">
+      <div className="card-back">
 					<h1>{props.name}</h1>
 					<hr />
 					<p>{props.details}</p>
 				<p className="item-price"><b>${props.price}</b></p>
         <Btn title = "More info" />
-				</div></div>
+				</div>
   </div>
 </div>
 

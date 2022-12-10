6f69d612f15e7d3af6bf606009b091280160a695
ReemaKusa <s12112910@stu.najah.edu>
2022-12-10 15:37:27 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
-
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Pages/Home';
 import Services from './components/Pages/Services';
-import AboutUs from './components/Pages/AboutUs';
 import {  Route, Routes } from 'react-router-dom';
 import Login from './components/Pages/LoginPage';
 import Register from './components/Pages/RegisterPage';
@@ -21,7 +18,6 @@ import Contact from './components/Pages/ContactUs';
         <Route path="/" element={<Home />}/>
           <Route path="/ContactUs" element={<Contact />} />
           <Route path="/Services" element={<Services />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
 

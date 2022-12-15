965f4d18408d68aee7fd651b409fc44246a7e10e
ReemaKusa <s12112910@stu.najah.edu>
2022-12-15 18:32:16 +0300

    HP design

---
+++ src/components/HomePageDesign/HPDesign.js
@@ -0,0 +1,19 @@
+//import
+import './HPDesign.css'
+//function
+
+const HPDesign =()=>{
+
+    return(
+
+<div className="main-p" >
+
+  <h5 className="design"> <span> Welcome </span> here </h5>
+
+</div>
+
+    );
+}
+
+//export
+export default HPDesign 

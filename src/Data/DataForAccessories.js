bf1363f436e041fcb42cf3326b11284ea3ad5b29
danaabdulhaq <danarabdulhaq@gmail.com>
2022-12-16 20:24:37 +0200

    data file

---
+++ src/Data/DataForAccessories.js
@@ -0,0 +1,31 @@
+
+
+import necklec from './prodectaccessories/necklec.jpg';
+import Ba from './prodectaccessories/Ba.jpg'
+import Ca from './prodectaccessories/Ca.jpg';
+import Nk from './prodectaccessories/Nk.jpg';
+import bab from './prodectaccessories/bab.jpg';
+import Cd from './prodectaccessories/Cd.jpg';
+import Nn from './prodectaccessories/Nn.jpg';
+import Bc from './prodectaccessories/Bc.jpg';
+import Cf from './prodectaccessories/Cf.jpg';
+import Nz from './prodectaccessories/Nz.jpg';
+
+const Accessories = [
+    
+
+            {color : 'silver' , url :  necklec ,  name : "necklec" , details : " size : S   " ,  price : 70 },
+            {color : 'silver' ,url :  Ba ,  name : "Bracelet" , details : " size : S    " ,  price : 50 },
+            {color : 'golden' ,url :  Ca ,  name : "Crown" , details : " size : M   " ,  price : 200 },
+            {color : 'golden' , url :  Nk ,  name : "necklec" , details : " size : L  " ,  price : 70 },
+            {color : 'golden', url :  bab ,  name : "Bracelet" , details : " size : S    " ,  price : 60 },
+            {color : 'golden' ,url :  Cd ,  name : "Crwon" , details : " size : L   " ,  price : 100 },
+            {color : 'golden' , url :  Nn ,  name : "necklec" , details : " size : M   " ,  price : 180 },
+            {color : 'silver', url :  Bc ,  name : "bracelet" , details : " size : S    " ,  price : 40 },
+            {color : 'golden' , url :  Cf ,  name : "Croen" , details : " size : L   " ,  price : 200 },
+            {color : 'silver' , url :  Nz ,  name : "necklec" , details : " size : L  " ,  price : 300 }
+
+]
+
+export default Accessories;
+
---
+++ src/Data/DataForFlowers.js
@@ -0,0 +1,32 @@
+
+import fa from './photo/fa.jpg';
+import fc from './photo/fc.jpg';
+import fd from './photo/fd.jpg';
+import fe from './photo/fe.jpg';
+import ff from './photo/ff.jpg';
+import fg from './photo/fg.jpg';
+import fh from './photo/fh.jpg';
+import fi from './photo/fi.jpg';
+import fj from './photo/fj.jpg';
+import fk from './photo/fk.jpg';
+
+
+const Flowers = [
+    
+{color : 'white' ,url :  fa ,  name : "Pink & White Bouquet" , details : " size : s  " ,  price : 70 },
+{color : 'Pink', url :  fc ,  name : "Pink  Bouquet" , details : " size : s    " ,  price : 100 },
+{color : 'Pink', url :  fd ,  name : "Pink & Green Bouquet" , details : " size : s    " ,  price : 350 },
+{color : 'Blue', url :  fe ,  name : "Blue & White Bouquet" , details : " size : s   " ,  price : 80 },
+{color : 'white',url :  ff ,  name : "Pink & White Bouquet" , details : " size : s    " ,  price : 40 },
+{color : 'Green' , url :  fg ,  name : "Green & White Bouquet" , details : " size : s    " ,  price :100 },
+{color : 'white', url :  fh ,  name : " White Bouquet" , details : " size : s   " ,  price : 100 },
+{color : 'Green', url :  fi ,  name : "Pink  Green& White Bouquet" , details : " size : s   " ,  price : 60 },
+{color : 'Blue', url :  fj,  name : "Blue & BabyGreen Bouquet" , details : " size : s   " ,  price : 100 },
+{color : 'Red' ,url :  fk ,  name : "Red Bouquet" , details : " size : s   " ,  price : 150 }
+
+
+
+]
+
+export default Flowers;
+

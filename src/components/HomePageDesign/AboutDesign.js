0f56cb8d84c5ed7d11ee7d70a3dd2f9d64ef404c
ReemaKusa <s12112910@stu.najah.edu>
2022-12-19 14:25:18 +0300

    About us design for HP

--- src/components/HomePageDesign/AboutDesign.js
+++ src/components/HomePageDesign/AboutDesign.js
@@ -1,8 +1,8 @@
 //import
 import React from "react";
-import aboutVid from './homeV/aboutVideo.mp4'
+import aboutVid from './images/aboutVideo.mp4'
 import './AboutDesign.css'
-import { Link } from "react-router-dom";
+
 
 //functions
 const AboutDesign=()=>{
@@ -22,7 +22,7 @@ return(
         <h3>Why la belle</h3>
         <p>We offer you the latest, most beautiful and elegant dresses, accessories and packages to get the most beautiful look on your special day.</p>
         <p>Best is yet to come, cherish each moment along this journey .</p>
-        <Link to="/AboutUs"className="btn">Learn more</Link>
+        <a href="..." className="btn">Learn more</a>
     </div>
 
  </div>

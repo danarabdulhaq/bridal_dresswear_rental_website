629680c3ab2f5f61f4aed981cfbf31f2e1ab7a48
unknown <natsheh.marah22@gmail.com>
2022-12-20 20:15:04 +0200

    filter

--- src/components/Data/DataForDresses.js
+++ src/components/Data/DataForDresses.js
@@ -11,12 +11,12 @@ import white_dantiel from '../Product_photos/white_dantiel.jpg';
 
 
                  [{color : 'white', url :  chiffon_dress ,  name : " chiffon dress" , details : " size : M / XS   " ,  price : 1500 },
-                    {color : 'white' , url :  fluffy_dress ,   name : " fluffy dress" ,  details : " size : XS    " ,  price : 1400 },
-                    {color : 'beige' , url :   High_neck_dress, name : " High neck dress", details : " size : S   " , price : 1600},
-                    {color : 'white' , url :   maria_dress ,     name :" maria dress" ,    details : " size : L / S  " , price : 1555 },
-                    {color : 'white' , url :   midriff ,         name :' midriff' ,        details : " size : M / L " ,  price : 1350},
-                    {color : 'white' ,url :   Puffed_sleeves ,      name :" Puffed sleeves" ,      details : " size : XL / L  " , price : 1500 },
-                    {color : 'white' , url :   white_dantiel ,       name :" white dantiel dress" , details : "size : M    " , price : 1200 },
+                    {color : 'white' , url :  fluffy_dress ,   name : " fluffy lace dress" ,  details : " size : XS    " ,  price : 1400 },
+                    {color : 'beige' , url :   High_neck_dress, name : " High satin neck dress", details : " size : S   " , price : 1600},
+                    {color : 'beige' , url :   maria_dress ,     name :" maria satin dress" ,    details : " size : L / S  " , price : 1555 },
+                    {color : 'white' , url :   midriff ,         name :' midriff chiffon dress' ,        details : " size : M / L " ,  price : 1350},
+                    {color : 'white' ,url :   Puffed_sleeves ,      name :"Puffed sleeves satin dress" ,      details : " size : XL / L  " , price : 1500 },
+                    {color : 'white' , url :   white_dantiel ,       name :" white dantiel lace dress" , details : "size : M    " , price : 1200 },
                  ] ;
                     
 

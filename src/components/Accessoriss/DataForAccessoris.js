7050f99fb9a62ffa806e8baf715c09990547dff3
EmanSOthman <eman.s.s.otman@gmail.com>
2022-12-20 18:30:05 +0200

    accessoris file

---
+++ src/components/Accessoriss/DataForAccessoris.js
@@ -0,0 +1,31 @@
+
+import necklec from './necklec.jpg';
+
+import Ca from './Ca.jpg';
+import Nk from './Nk.jpg';
+import bab from './bab.jpg';
+import Cd from './Cd.jpg';
+
+import Bc from './Bc.jpg';
+
+
+
+
+
+const DataForAccessoris =
+
+
+                [{ color : 'silver',url :  necklec ,  name : "necklec" , details : " size : s  " ,  price : "70" },
+                
+                { color :'golden',url :  Ca ,  name : "Crown" , details : " size : M    " ,  price : "200" },
+                {color : 'silver',url :  Nk ,  name : "necklec" , details : " size : L  " ,  price : "70" },
+                {color :'golden',url :  bab ,  name : "Bracelet" , details : " size : s    " ,  price : "60" },
+                { color :'golden',url :  Cd ,  name : "Crwon" , details : " size : L   " ,  price : "350" },
+                
+                {color:'silver',url :  Bc ,  name : "bracelet" , details : " size : s    " ,  price : "40" },
+                
+
+
+
+];
+export default DataForAccessoris ;

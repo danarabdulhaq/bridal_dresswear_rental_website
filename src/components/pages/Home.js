bb0d69226ed6d95d81e54b319f7f0d46fc8e55e7
danaabdulhaq <danarabdulhaq@gmail.com>
2022-12-22 20:47:31 +0200

    last

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,25 +1,19 @@
 import React from "react";
 import Slider from "../Slider/Slider"
-import HomeDesign from '../HomePageDesign/AboutDesign'
-import CardMain from "../Card-main/card-main";
-import FeedBack from '../FeedBack/FeedBack'
-import HPDesign from '../HomePageDesign/HPDesign'
-
 
 
 const Home=()=>
 {
 return(
 <>
-    <Slider/>
-    <HPDesign/>
-    <div className="mx-5">
-        <CardMain/></div>
-            <div className="my-5" id="vid">
-                <HomeDesign/>
-            </div>
-                    <FeedBack/>
-
+<Slider/>
 </>
-)}
+
+
+)
+
+
+
+
+}
 export default Home;
--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -4,31 +4,31 @@ import '../Form/Form'
 const Register = ()=> { 
   
 return (
-<div className='register' >  
+<div className='register' style={{backgroundColor: "#9174a3"}}>  
    <Form text= "Sign into your account"> 
    <div className="form-outline mb-4 ">
         <input type="text" id="name" placeholder=" "className="text-input" autoComplete='off' required/>
-        <label className="form-label" for="name">Full name</label>
+        <label className="label" for="name">Full name</label>
       </div>
       <div className="form-outline mb-4">
         <input type="text" id="city" placeholder=" " className="text-input" autoComplete='off' required/>
-        <label className="form-label" for="city">City</label>
+        <label className="label" for="city">City</label>
       </div>
       <div className="form-outline mb-4">
         <input type="text" id="address" placeholder=" " className="text-input" autoComplete='off' required/>
-        <label className="form-label" for="address">Address</label>
+        <label className="label" for="address">Address</label>
       </div>
       <div className="form-outline mb-4">
         <input type="tel" id="phone" placeholder=" " className="text-input" autoComplete='off' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/>
-        <label className="form-label" for="phone">Phone number</label>
+        <label className="label" for="phone">Phone number</label>
       </div>
       <div className="form-outline mb-4">
         <input type="email" id="email" placeholder=" " className="text-input" autoComplete='off' required/>
-        <label className="form-label" for="email">Email</label>
+        <label className="label" for="email">Email</label>
       </div>
       <div className="form-outline mb-4">
         <input type="password" id="pass" placeholder=" " className="text-input" autoComplete='off' required/>
-        <label className="form-label" for="pass">password</label>
+        <label className="label" for="pass">password</label>
       </div>
     </Form>
 

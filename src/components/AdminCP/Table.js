fd2e0e0d3f3e66e9608d281c6a265dc3beca1217
unknown <natsheh.marah22@gmail.com>
2022-12-24 21:53:48 +0200

    Dashboard Tables

---
+++ src/components/AdminCP/Table.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+const Table=(props)=>{
+
+return(
+<table class="table table-striped">
+    <tr  class="bg">
+        <th>
+            
+        </th>
+        <th>details</th>
+        <th>Color</th>
+        <th>Price</th>
+        <th>Action</th>
+    </tr>
+
+    <tbody id="myTable">
+     {props.children} 
+    </tbody>
+</table>
+);
+
+
+
+}
+export default Table;

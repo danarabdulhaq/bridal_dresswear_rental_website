12c020b6e79486d386e16135450a002e333929a2
EmanSOthman <eman.s.s.otman@gmail.com>
2022-12-24 19:55:03 +0200

    rename file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {  Route, Routes } from 'react-router-dom';
 import Login from './components/Pages/LoginPage';
 import Register from './components/Pages/RegisterPage';
 import Contact from './components/Pages/ContactUs';
-// import About from './components/Pages/Aboutus'
+//import About from './components/Pages/Aboutus'
 import DisplayDresses from './components/PageDresses/DisplayDresses';
 import Displayaccessories from './components/Accessoris/Displayaccessories';
 import DisplayFlower from './components/Flower/DisplayFlower'
@@ -19,45 +19,45 @@ import Admin from './components/Pages/AdminLog';
 import Calendaer from './components/Calendar/Calendar.js'
 
  function App() {
-//   <Services/>
-//    const [loading, setLoading] = useState(false);
-//    useEffect(() => {
-//      setLoading(true);
-//      setTimeout(() => {
-//        setLoading(false);
-//      }, 5300);
-//    }, []);
-//    return (
-//     <div className='App'>
-//       {loading ? (
-//  <div className="loader-container">
-//  <Preloader />
-//  </div>
-//  ) : (
-//    <div className="main-content">
-//      <Navbar/>
-//        <Routes>
-//          <Route path="/" element={<Home />}/>
-//            <Route path="/ContactUs" element={<Contact />} />
-//            <Route path="/Services" element={<Services />} />
-//            {/* <Route path="/AboutUs" element={<About  />} /> */}
-//           <Route path="/Login" element={<Login />} />
-//            <Route path="/Register" element={<Register />} />
-//            <Route path="/DisplayDress" element={<DisplayDresses/>}/>
-//            <Route path="/Displayaccessories" element={<Displayaccessories />}/>
-//            <Route path="/DisplayFlower" element={<DisplayFlower/>}  />
-//           <Route path="/Admin" element={<Admin/>}  />
+  <Services/>
+   const [loading, setLoading] = useState(false);
+   useEffect(() => {
+     setLoading(true);
+     setTimeout(() => {
+       setLoading(false);
+     }, 5300);
+   }, []);
+   return (
+    <div className='App'>
+      {loading ? (
+ <div className="loader-container">
+ <Preloader />
+ </div>
+ ) : (
+   <div className="main-content">
+     <Navbar/>
+       <Routes>
+         <Route path="/" element={<Home />}/>
+           <Route path="/ContactUs" element={<Contact />} />
+           <Route path="/Services" element={<Services />} />
+           {/* <Route path="/AboutUs" element={<About  />} /> */}
+          <Route path="/Login" element={<Login />} />
+           <Route path="/Register" element={<Register />} />
+           <Route path="/DisplayDress" element={<DisplayDresses/>}/>
+           <Route path="/Displayaccessories" element={<Displayaccessories />}/>
+           <Route path="/DisplayFlower" element={<DisplayFlower/>}  />
+          <Route path="/Admin" element={<Admin/>}  />
           <Route path="/Calendaer" element={<Calendaer/>}  />
           
 
           
           
-//        </Routes>
-//      <Footer/>
+       </Routes>
+     <Footer/>
 
 
-//  </div>)}
-//  </div>
-//  );
+ </div>)}
+ </div>
+ );
 }
 export default App;

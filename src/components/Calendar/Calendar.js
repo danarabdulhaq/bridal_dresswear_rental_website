0df8e3f63dfd22d42be71cc3dce9f6c5e80a3a72
tareq <s12112542@stu.najah.edu>
2023-01-13 13:29:40 +0200

    change calendar logic

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,42 +1,74 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import React, {  useRef, useState } from 'react';
 import './Calendar.css'
-import '../Register-btn/Register-btn.css'
+
 
 const Cal=()=> {
-  const [bookedDates, setBookedDates] = useState([new Date('2023-01-01'), new Date('2023-03-01')]);
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const startDate=useRef();
+  const endDate=useRef();
+  const today=new Date();
+  const bookedDates=useState([[new Date('01-13-2023'),new Date('01-15-2023')],[new Date('01-17-2023'),new Date('01-20-2023')]]);
+  const [book,setBook]=useState()
 
-  function handleChange(dateRange) {
-    setStartDate(dateRange[0]);
-    setEndDate(dateRange[1]);
+  const handelClick=()=>{
+    const start = startDate.current.valueAsDate;
+    const end = endDate.current.valueAsDate;
+
+    let error = "";
+
+    // Iterate over the bookedDates state array
+    bookedDates[0].forEach(range => {
+        const bookedStart = range[0];
+        const bookedEnd = range[1];
+
+        // Check if the selected start date is within the booked range
+        if (start >= bookedStart && start <= bookedEnd) {
+            error = "Selected start date is already booked.";
+        }
+        // Check if the selected end date is within the booked range
+        if (end >= bookedStart && end <= bookedEnd) {
+            error = "Selected end date is already booked.";
+        }
+        // Check if the selected range completely overlaps the booked range
+        if (start < bookedStart && end > bookedEnd) {
+            error = "Selected dates are already booked.";
+        }
+    });
+if(end<start)
+  {
+    console.log('invalid date')
+    setBook(false)
+    return
   }
+    if (error) {
+        // Show the error message
+        
+        console.error(error);
+    } else {
+        // Proceed with booking the dates
+        setBook(true)
+        console.log("Dates booked successfully!");
+        bookedDates.push([startDate,endDate])
 
-  function handleBooking() {
-    const bookedDatesInRange = [];
-    let currentDate = startDate;
-    while (currentDate <= endDate) {
-      if (bookedDates.includes(currentDate)) {
-        alert('One or more of the selected dates are already booked.');
-        return;
-      }
-      bookedDatesInRange.push(currentDate);
-      currentDate = new Date(currentDate.getTime() );
     }
-    setBookedDates([...bookedDates, ...bookedDatesInRange]);
+
+
   }
+
+  
   return (
-    <div className='mx-5' id='calDiv'>
-      <Calendar
-      locale="en-US"
-        onChange={handleChange}
-        selectRange
-        value={[startDate, endDate]}
-        tileDisabled={date => bookedDates.includes(date)}
-      />
-      <button id="registerBtn" onClick={handleBooking}>Book dates</button>
+    <>
+    <label>from</label>
+    <input type="date" min={today.toISOString().slice(0, 10)}ref={startDate}  />
+    <label>to</label>
+    <input type='date' min={today.toISOString().slice(0, 10)}ref={endDate}/>
+    <button onClick={handelClick}id="registerBtn">Book date</button>
+    {book?(
+      <div className="alert  alert-dark mx-5 my-3" role="alert">
+      Dates booked successfully
     </div>
+    ):(<div className="alert alert-danger mx-5 my-3" role="alert">
+    Selected dates are already booked.  </div>)}
+    </>
   );
 }
 export default Cal;
---
+++ src/components/Calendar/CalenderView.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Appointment=()=>
+{
+return(
+    <>
+    <div className="control-section">
+    <div className="content-wrapper">
+        <div id="Schedule"></div>
+    </div>
+</div>
+    </>
+)
+
+
+}
+
+export default Appointment;

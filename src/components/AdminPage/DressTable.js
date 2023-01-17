d006a258639384e0f8432611d5d3cac2254dda13
tareq <s12112542@stu.najah.edu>
2023-01-17 19:43:15 +0200

    change admin page

---
+++ src/components/AdminPage/DressTable.js
@@ -0,0 +1,25 @@
+import Admin from "./Admin";
+import SideBar from "./Sidebar";
+import ToolBar from "./ToolBar";
+
+
+
+const DressTable=()=>{
+
+
+    return (
+        <>
+       <ToolBar/>
+        <div className="row mx-0">
+                        <SideBar/>
+            <div className="col">
+                        <h1>dress</h1>
+</div>
+
+        
+
+        </div>
+        </>
+    )
+}
+export default DressTable;
---
+++ src/components/AdminPage/ToolBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+function ToolBar () {
+
+
+
+
+
+    
+
+    return (
+
+        
+        <div className='toolBar'>
+            <div className='burger'>
+                <button>
+            </button>
+            </div>
+            <div className='title'>
+                Admin control panel
+            </div>
+
+        </div>
+        
+       
+    );
+}
+
+
+
+
+
+
+export default ToolBar 
+
